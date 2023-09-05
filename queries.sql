f97a9245031a0533abfa8788ebaf0cd87d7c8247
AnnaIShulga <144025562+AnnaIShulga@users.noreply.github.com>
2023-09-05 20:50:52 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -31,3 +31,41 @@ left join employees e on e.employee_id = s.sales_person_id
 left join products p on p.product_id = s.product_id
 group by CONCAT(e.last_name,' ', e.first_name),s.sale_date
 order by to_char(s.sale_date, 'D'), name;
+
+select case when age > '16' and age <='25' then '16-25' --количество покупателей в разных возрастных группах: 16-25, 26-40 и 40+
+			when age >='26' and age <='40' then '26-40'
+			when age >='41' then '40+'
+			else 'other'
+		end as age_category,
+		count(distinct c.customer_id)
+from customers c 
+group by age_category
+
+select --данные по количеству уникальных покупателей и выручке, которую они принесли
+	to_char(s.sale_date, 'YYYY-MM') as date,
+	count(distinct s.customer_id) as total_customers,
+	round(sum(s.quantity * p.price),0)as income
+from sales s
+left join products p on p.product_id = s.product_id
+group by to_char(s.sale_date, 'YYYY-MM')
+order by to_char(s.sale_date, 'YYYY-MM');
+
+select --покупатели первая покупка которых пришлась на время проведения специальных акций
+	customer,
+	sale_date,
+	seller
+from	(select 
+		concat(c.last_name,' ',c.first_name) as customer,
+		s.sale_date as sale_date,
+		concat(e.last_name,' ',e.first_name) as seller,
+		sum(s.quantity * p.price) as income,
+		c.customer_id as id,
+		row_number () over (partition by c.customer_id order by s.sale_date) as rn
+		from sales s
+		left join employees e on e.employee_id = s.sales_person_id
+		left join products p on p.product_id = s.product_id
+		left join customers c on c.customer_id = s.customer_id
+		group by concat(c.last_name,' ',c.first_name),s.sale_date,concat(e.last_name,' ',e.first_name),c.customer_id ) as total
+where rn = '1' and income = '0'
+order by id
+	
